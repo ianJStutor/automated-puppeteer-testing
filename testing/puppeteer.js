07a2fed4f7c8025f314c7bca1a489b612a3bbe94
Ian Marshall <ian.js.tutor@gmail.com>
2022-12-07 11:27:01 -0500

    changed testrunner naming conventions

--- testing/puppeteer.js
+++ testing/puppeteer.js
@@ -1,17 +1,16 @@
 import puppeteer from "puppeteer";
-import {test, expect} from "../lib/testrunner.js";
+import {test, testrunner} from "../lib/testrunner.js";
 import assert from "node:assert/strict";
 
 async function runTests(HOST, PORT) {
 
-	// Test runner
-	test("Puppeteer e2e testing", async () => {
+	testrunner("Puppeteer e2e testing", async () => {
 
 		// Start Puppeteer
 		const browser = await puppeteer.launch();
 		const page = await browser.newPage();
 
-		await expect("Reservation for Rafael has expected confirmation message", async () => {
+		await test("Reservation for Rafael has expected confirmation message", async () => {
 			await page.goto(`http://${HOST}:${PORT}`);
 			await page.type("#name", "Rafael");
 			await page.select("#reservation", "Sat03");
@@ -21,7 +20,7 @@ async function runTests(HOST, PORT) {
 
 		});
 
-		await expect("Reservation for Zoya has expected caffeine-free confirmation message", async () => {
+		await test("Reservation for Zoya has expected caffeine-free confirmation message", async () => {
 			await page.goto(`http://${HOST}:${PORT}`);
 			await page.type("#name", "Zoya");
 			await page.select("#reservation", "Sun12");
@@ -29,7 +28,7 @@ async function runTests(HOST, PORT) {
 			await page.click("form button");
 			await page.waitForSelector("#confirmation");
 			// "Zoya" assertion test
-			
+
 		});
 
 		// Close Puppeteer
